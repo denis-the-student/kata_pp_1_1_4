6ad044388cab200cba259a852c263338158c0092
Denis Yanbakhtov <denis.the.student@gmail.com>
2023-03-27 14:05:23 +0300

    fix Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,13 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
         userService.saveUser("Name1", "Lastname1", (byte) 11);
         userService.saveUser("Name2", "Lastname2", (byte) 22);
         userService.saveUser("Name3", "Lastname3", (byte) 33);
